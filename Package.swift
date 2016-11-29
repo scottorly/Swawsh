f831ae67af126268fd85652d7932569a4102dd36
Scott Orlyck <sorlyck@pivotal.io>
2016-11-29 14:31:49 -0800

    Escape dependency hell
    Signed-off-by: Scott Orlyck <sorlyck@pivotal.io>

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Swawsh",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0, minor: 7)
+	.Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion:0, minor:8)
     ]
-    
 )
