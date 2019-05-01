afa2d239b707de56d97b2b7e722e420a9dc1f724
ozgunawesome <11738322+ozgunawesome@users.noreply.github.com>
2019-05-01 15:52:50 -0700

    updated for swift 4.2

--- Package.swift
+++ Package.swift
@@ -1,8 +1,26 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "Swawsh",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Swawsh",
+            targets: ["Swawsh"]),
+    ],
     dependencies: [
-	.Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion:0, minor:8)
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "0.8.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Swawsh",
+            dependencies: ["Cryptor"],
+            path: "Sources"),
     ]
 )
